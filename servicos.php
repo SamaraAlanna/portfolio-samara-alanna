3a656b751f5e0bd29e50790607e404075792ed17
Samara Alanna <sami_andrade@outlook.com.br>
2026-05-21 22:35:27 -0300

    Adiciona mensagem pré-preenchida no link WhatsApp do Site Acessível

--- servicos.php
+++ servicos.php
@@ -210,7 +210,7 @@
               <div class="servico-price">R$800</div>
               <div class="servico-prazo">Prazo: 7–10 dias úteis</div>
             </div>
-            <a href="https://wa.me/5541920058670" class="btn-servico" target="_blank">Solicitar serviço</a>
+            <a href="https://wa.me/5541920058670?text=Ol%C3%A1%2C%20vim%20pelo%20site%20e%20tenho%20interesse%20no%20Site%20Acess%C3%ADvel!" class="btn-servico" target="_blank">Solicitar serviço</a>
           </div>
         </div>
 
